156626dbb62a7798424b616bbc432aa3a6be7936
Joe McCleary <joemccleary@hotmail.co.uk>
2021-05-10 14:19:21 +0100

    added approval card format

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import faker from 'faker';
 
 const CommentDetail = (props) => {
     return (
         <div className="comment">
                 <a href="/" className="avatar">
-                    <img alt="logo" src={faker.image.avatar()}/>
+                    <img alt="logo" src={props.avatar}/>
                 </a>
                 <div className="content">
                     <a href="/" className="author">
--- src/index.js
+++ src/index.js
@@ -1,28 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import CommentDetail from './CommentDetail'
+import faker from 'faker'
+import ApprovalCard from './ApprovalCard'
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <div>
+            <ApprovalCard>
                 <CommentDetail 
                     author="Dave"
                     time="Today at 05.30"
                     message="Howdy"
+                    avatar={faker.image.avatar()}
                 />
+            </ApprovalCard>
+            <ApprovalCard>
                 <CommentDetail 
                     author="Sam"
                     time="Today at 07.31"
                     message="Gooday mate"
+                    avatar={faker.image.avatar()}
+
                 />
+            </ApprovalCard>
+            <ApprovalCard>
                 <CommentDetail 
                     author="Daniel"
                     time="Today at 15.20"
                     message="Good morrow"
+                    avatar={faker.image.avatar()}
+
                 />
-            </div>
-            
+            </ApprovalCard>          
         </div>
     );
 };
