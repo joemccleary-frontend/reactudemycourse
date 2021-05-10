c33cf0dd022555cfafe70d7b1067db47bf780f97
Joe McCleary <joemccleary@hotmail.co.uk>
2021-05-10 12:44:28 +0100

    installed faker js module, added 3 posts to idex,js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,60 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import faker from 'faker'
 
 const App = () => {
-    return <div>Hi there</div>
+    return (
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="logo" src={faker.image.avatar()}/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Tom
+                    </a>
+                    <div className="metadata">
+                        <span className="date">
+                            Yesterday at 5.02pm
+                        </span>
+                    </div>
+                    <div className="text">Nice ball shane</div>
+                </div>
+            </div>
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="logo" src={faker.image.avatar()}/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Tom
+                    </a>
+                    <div className="metadata">
+                        <span className="date">
+                            Yesterday at 5.02pm
+                        </span>
+                    </div>
+                    <div className="text">Nice ball shane</div>
+                </div>
+            </div>
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="logo" src={faker.image.avatar()}/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Tom
+                    </a>
+                    <div className="metadata">
+                        <span className="date">
+                            Yesterday at 5.02pm
+                        </span>
+                    </div>
+                    <div className="text">Nice ball shane</div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root') )
