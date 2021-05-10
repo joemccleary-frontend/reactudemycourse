66cc3f8d374a83a00c9d848518711afecfce97e2
Joe McCleary <joemccleary@hotmail.co.uk>
2021-05-10 13:22:03 +0100

    commentdetail file added, props parsed through commentdetail call

--- src/index.js
+++ src/index.js
@@ -1,58 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import faker from 'faker'
+import CommentDetail from './CommentDetail'
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="logo" src={faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Tom
-                    </a>
-                    <div className="metadata">
-                        <span className="date">
-                            Yesterday at 5.02pm
-                        </span>
-                    </div>
-                    <div className="text">Nice ball shane</div>
-                </div>
+            <div>
+                <CommentDetail 
+                    author="Dave"
+                    time="Today at 05.30"
+                    message="Howdy"
+                />
+                <CommentDetail 
+                    author="Sam"
+                    time="Today at 07.31"
+                    message="Gooday mate"
+                />
+                <CommentDetail 
+                    author="Daniel"
+                    time="Today at 15.20"
+                    message="Good morrow"
+                />
             </div>
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="logo" src={faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Tom
-                    </a>
-                    <div className="metadata">
-                        <span className="date">
-                            Yesterday at 5.02pm
-                        </span>
-                    </div>
-                    <div className="text">Nice ball shane</div>
-                </div>
-            </div>
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="logo" src={faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Tom
-                    </a>
-                    <div className="metadata">
-                        <span className="date">
-                            Yesterday at 5.02pm
-                        </span>
-                    </div>
-                    <div className="text">Nice ball shane</div>
-                </div>
-            </div>
+            
         </div>
     );
 };
